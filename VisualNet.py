21657bf427b117186556b17dfcad99571fb79ab0
jlee <junyeop.lee@kt.com>
2017-07-26 08:16:11 +0900

    Modify Visability fc layer 3 to 2

--- VisualNet.py
+++ VisualNet.py
@@ -35,7 +35,6 @@ class VisualNet(nn.Module):
 			nn.ReLU(inplace=True),
 		)
 
-#self.conv5_pose = vgg.make_layers(['M',512,512,512,'M'])
 		self.conv5_pose = nn.Sequential(
 			nn.MaxPool2d(kernel_size=2, stride=2),
 			nn.Conv2d(512, 512, kernel_size=3, padding=1),
@@ -55,34 +54,41 @@ class VisualNet(nn.Module):
 			nn.ReLU(True),
 			nn.Dropout(),
 		)
-		self.landmark_vis_loc_1 = nn.Linear(4096,6)
-		self.landmark_vis_loc_2 = nn.Linear(4096,6)
-		self.landmark_vis_loc_3 = nn.Linear(4096,6)
-		self.landmark_vis_loc_4 = nn.Linear(4096,6)
+		self.landmark_vis_1 = nn.Linear(4096,2)
+		self.landmark_vis_2 = nn.Linear(4096,2)
+		self.landmark_vis_3 = nn.Linear(4096,2)
+		self.landmark_vis_4 = nn.Linear(4096,2)
+		self.landmark_vis_5 = nn.Linear(4096,2)
+		self.landmark_vis_6 = nn.Linear(4096,2)
+		self.landmark_vis_7 = nn.Linear(4096,2)
+		self.landmark_vis_8 = nn.Linear(4096,2)
+
+		self.landmark_loc_1 = nn.Linear(4096,4)
+		self.landmark_loc_2 = nn.Linear(4096,4)
+		self.landmark_loc_3 = nn.Linear(4096,4)
+		self.landmark_loc_4 = nn.Linear(4096,4)
 		
 		self._initialize_weights()
-#self.landmark_visibility2 = nn.Linear(1024,2)
-#		self.landmark_visibility3 = nn.Linear(1024,2)
-#		self.landmark_visibility4 = nn.Linear(1024,2)
-
-#		self.landmark_locationx1 = nn.Linear(1024,4)
-#		self.landmark_locationx2 = nn.Linear(1024,4)
-#		self.landmark_locationx3 = nn.Linear(1024,4)
-#		self.landmark_locationx4 = nn.Linear(1024,4)
-
-#self._init_weights()
 
 	def forward(self, x):
 		feature = self.features(x)
 		l = self.conv5_pose(feature) 
 		l = l.view(l.size(0), -1)
 		l = self.fc6_7(l)
-		l1 = self.landmark_vis_loc_1(l)
-		l2 = self.landmark_vis_loc_2(l)
-		l3 = self.landmark_vis_loc_3(l)
-		l4 = self.landmark_vis_loc_4(l)
+		v1 = self.landmark_vis_1(l)
+		v2 = self.landmark_vis_2(l)
+		v3 = self.landmark_vis_3(l)
+		v4 = self.landmark_vis_4(l)
+		v5 = self.landmark_vis_5(l)
+		v6 = self.landmark_vis_6(l)
+		v7 = self.landmark_vis_7(l)
+		v8 = self.landmark_vis_8(l)
 
-		return l1,l2,l3,l4
+		l1 = self.landmark_loc_1(l)
+		l2 = self.landmark_loc_2(l)
+		l3 = self.landmark_loc_3(l)
+		l4 = self.landmark_loc_4(l)
+		return v1,v2,v3,v4,v5,v6,v7,v8,l1,l2,l3,l4
 
 	def _initialize_weights(self):
 		for m in self.modules():
@@ -98,17 +104,6 @@ class VisualNet(nn.Module):
 				n = m.weight.size(1)
 				m.weight.data.normal_(0, 0.01)
 				m.bias.data.zero_()
-
-#lv1 = self.landmark_visibility1(l)
-#		lv2 = self.landmark_visibility2(l)
-#		lv3 = self.landmark_visibility3(l)
-#		lv4 = self.landmark_visibility4(l)
-
-#		ll1 = self.landmark_locationx1(l)
-#		ll2 = self.landmark_locationx2(l)
-#		l13 = self.landmark_locationx3(l)
-#		l14 = self.landmark_locationx4(l)
-
 
 def vsnet():
 	model = VisualNet(vgg.vgg16(pretrained = True))
