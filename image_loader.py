e006ca0582efcc04d97e185e3398913b5bb36699
jlee <junyeop.lee@kt.com>
2017-07-26 08:34:26 +0900

    Load clothes_type column

--- image_loader.py
+++ image_loader.py
@@ -29,25 +29,25 @@ class ImageFolder(data.Dataset):
 		self.target_transform = target_transform
 		self.loader = loader
 
-		self.data_path = data_frame['image_name'].values.tolist()
+		self.image_path = data_frame['image_name'].values
+		self.clothes_type = data_frame['clothes_type'].values
 		self.data_vis_loc = data_frame[[
-							'landmark_visibility_1','landmark_location_x_1','landmark_location_y_1',
-							'landmark_visibility_2','landmark_location_x_2','landmark_location_y_2',
-							'landmark_visibility_3','landmark_location_x_3','landmark_location_y_3',
-							'landmark_visibility_4','landmark_location_x_4','landmark_location_y_4',
-							'landmark_visibility_5','landmark_location_x_5','landmark_location_y_5',
-							'landmark_visibility_6','landmark_location_x_6','landmark_location_y_6',
-							'landmark_visibility_7','landmark_location_x_7','landmark_location_y_7',
-							'landmark_visibility_8','landmark_location_x_8','landmark_location_y_8',
+							'landmark_visibility_1','landmark_visibility_2','landmark_visibility_3','landmark_visibility_4',
+							'landmark_visibility_5','landmark_visibility_6','landmark_visibility_7','landmark_visibility_1',
+							'landmark_location_x_1','landmark_location_y_1','landmark_location_x_2','landmark_location_y_2',
+							'landmark_location_x_3','landmark_location_y_3','landmark_location_x_4','landmark_location_y_4',
+							'landmark_location_x_5','landmark_location_y_5','landmark_location_x_6','landmark_location_y_6',
+							'landmark_location_x_7','landmark_location_y_7','landmark_location_x_8','landmark_location_y_8',
 		]].values
 
 	def __getitem__(self, index):
-		path = self.data_path[index]
+		path = self.image_path[index]
+		clothes_type = self.clothes_type[index]
 		target = self.data_vis_loc[index]
 		img = self.loader(self.root + path)
 		if self.transform is not None:
 			img = self.transform(img)
-		return (img, target)
+		return (img, ,clothes_type, target)
 
 	def __len__(self):
 		return len(self.data_path)
