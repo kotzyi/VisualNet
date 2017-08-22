ebabb299af1bc8b9db837965e96e109fafa1a001
jlee <junyeop.lee@kt.com>
2017-08-22 17:39:20 +0900

    Complete triplet training code

--- triplet_train.py
+++ triplet_train.py
@@ -94,8 +94,6 @@ def main():
 		land_model = torch.nn.DataParallel(land_model).cuda()
 		visual_model = torch.nn.DataParallel(visual_model).cuda()
 	
-#triplet_model = tripletnet(visual_model)
-#triplet_model = torch.nn.DataParallel(triplet_model).cuda()
 	# optionally resume from a checkpoint
 	if args.land_load:
 		if os.path.isfile(args.land_load):
@@ -155,22 +153,22 @@ def main():
 	for epoch in range(args.start_epoch, args.epochs):
 		adjust_learning_rate(optimizer, epoch)
 		# train for one epoch
-		train(image_data, model, softmax, l1loss, optimizer, epoch)
+		train(triplet_data, land_model, visual_model, triplet_loss, optimizer)
 		# evaluate on validation set
 		#prec1 = validate(val_loader, model, criterion)
 
 	    # remember best prec@1 and save checkpoint
 		#		is_best = prec1 > best_prec1
 		#		best_prec1 = max(prec1, best_prec1)
-		save_checkpoint({
-			'epoch': epoch + 1,
-			'arch': 'visualnet', 
-			'state_dict': model.state_dict(),
+#save_checkpoint({
+#			'epoch': epoch + 1,
+#			'arch': 'visualnet', 
+#			'state_dict': model.state_dict(),
 		#'best_prec1': best_prec1,
 		#}, is_best)
-		},True)
+#		},True)
 
-def validate(triplet_data, land_model, visual_model, triplet_loss, optimizer):
+def train(triplet_data, land_model, visual_model, triplet_loss, optimizer):
 	land_model.eval()
 	visual_model.train()
 
@@ -205,122 +203,6 @@ def validate(triplet_data, land_model, visual_model, triplet_loss, optimizer):
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
-
-		
-
-#score = visual_model(feature, pool5_layer)
-#		print(score)
-
-'''
-def validate(val_loader, land_model, visual_model, softmax, l1loss):
-	batch_time = AverageMeter()
-	data_time = AverageMeter()
-	losses_v = AverageMeter()
-	losses_l = AverageMeter()
-		       top1 = AverageMeter()
-		       top5 = AverageMeter()
-		       # switch to train mode
-		       land_model.eval()
-	    visual_model.eval()
-
-	    end = time.time()
-	    for i, (input, clothes_type, collar, sleeve, waistline, hem, path) in enumerate(val_loader):
-			        # measure data loading time
-			        data_time.update(time.time() - end)
-	        input_var = Variable(input)
-	        # compute output
-	        vis_collar, vis_sleeve, vis_waistline, vis_hem, collar_out, sleeve_out, waistline_out, hem_out, feature = land_model(input_var)
-
-	        pool5_layer = gen_pool5_layer((vis_collar[:,0:3], vis_collar[:,3:6],vis_sleeve[:,0:3], vis_sleeve[:,3:6],
-						                    vis_waistline[:,0:3], vis_waistline[:,3:6],vis_hem[:,0:3], vis_hem[:,3:6]),
-					                    (collar_out[:,0:2], collar_out[:,2:4], sleeve_out[:,0:2], sleeve_out[:,2:4],
-										                      waistline_out[:,0:2], waistline_out[:,2:4], hem_out[:,0:2], hem_out[:,2:4]), feature)
-
-	        score = visual_model(feature, pool5_layer)
-	        print(score)
-'''
-
-
-def train(train_loader, model, softmax, l1loss, optimizer, epoch):
-	'''
-	In clothes type, 
-	"1" represents upper-body clothes, 
-	"2" represents lower-body clothes, 
-	"3" represents full-body clothes. 
-	Upper-body clothes possess six fahsion landmarks, 
-	lower-body clothes possess four fashion landmarks, 
-	full-body clothes possess eight fashion landmarks.
-	For upper-body clothes, landmark annotations are listed in the order of ["left collar", "right collar", "left sleeve", "right sleeve", "left hem", "right hem"]
-	For lower-body clothes, landmark annotations are listed in the order of ["left waistline", "right waistline", "left hem", "right hem"]
-	For upper-body clothes, landmark annotations are listed in the order of ["left collar", "right collar", "left sleeve", "right sleeve", "left waistline", "right waistline", "left hem", "right hem"].
-	'''
-	batch_time = AverageMeter()
-	data_time = AverageMeter()
-	losses_v = AverageMeter()
-	losses_l = AverageMeter()
-	top1 = AverageMeter()
-	top5 = AverageMeter()
-	# switch to train mode
-	model.train()
-	end = time.time()
-	for i, (input, clothes_type, collar, sleeve, waistline, hem) in enumerate(train_loader):
-		# measure data loading time
-		data_time.update(time.time() - end)
-		input_var = Variable(input)
-		#target_var = Variable(target).float().cuda(async=True)
-		# compute output
-		collar_out, sleeve_out, waistline_out, hem_out, _ = model(input_var)
-
-		#		collar_indice = Variable(torch.LongTensor([idx for idx, x in enumerate(collar) if x == 1 or x == 3])).cuda()
-		#sleeve_indice = collar_indice
-		#waistline_indice = Variable(torch.LongTensor([idx for idx, x in enumerate(clothes_type) if x == 2 or x == 3])).cuda()
-	
-		# Answers
-		collar = Variable(collar).float().cuda(async=True)
-		sleeve = Variable(sleeve).float().cuda(async=True)
-		waistline = Variable(waistline).float().cuda(async=True)
-		hem = Variable(hem).float().cuda(async=True)
-
-		hem_vis_loss = softmax(hem_out[:,0:3], hem[:,0].long()) + softmax(hem_out[:,3:6], hem[:,1].long())
-		hem_land_loss = l1loss(hem_out[:,6:8], hem[:,2:4]) + l1loss(hem_out[:,8:10], hem[:,4:6])
-
-		collar_vis_loss = softmax(collar_out[:,0:3], collar[:,0].long()) + softmax(collar_out[:,3:6], collar[:,1].long())
-		collar_land_loss =l1loss(collar_out[:,6:8], collar[:,2:4]) + l1loss(collar_out[:,8:10], collar[:,4:6])
-
-		sleeve_vis_loss = softmax(sleeve_out[:,0:3], sleeve[:,0].long()) + softmax(sleeve_out[:,3:6], sleeve[:,1].long())
-		sleeve_land_loss = l1loss(sleeve_out[:,6:8], sleeve[:,2:4]) + l1loss(sleeve_out[:,8:10], sleeve[:,4:6])
-
-		waistline_vis_loss = softmax(waistline_out[:,0:3], waistline[:,0].long()) + softmax(waistline_out[:,3:6], waistline[:,1].long())
-		waistline_land_loss = l1loss(waistline_out[:,6:8], waistline[:,2:4]) + l1loss(waistline_out[:,8:10], waistline[:,4:6])
-		
-		vis_loss = hem_vis_loss + collar_vis_loss + sleeve_vis_loss + waistline_vis_loss
-		land_loss = hem_land_loss + collar_land_loss + sleeve_land_loss + waistline_land_loss
-		loss = land_loss + vis_loss
-		# measure accuracy and record loss
-		prec1, _ = accuracy(hem_out.data[:,0:3], hem.long().data[:,0].contiguous(), topk=(1, 1))
-		losses_v.update(vis_loss.data[0], input.size(0))
-		losses_l.update(land_loss.data[0], input.size(0))
-		top1.update(prec1[0], input.size(0))
-		#top5.update(prec5[0], input.size(0))
-
-	    # compute gradient and do SGD step
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-
-		# measure elapsed time
-		batch_time.update(time.time() - end)
-		end = time.time()
-
-		if i % args.print_freq == 0:
-			print('Epoch: [{0}][{1}/{2}]\t'
-				'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-				'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-				'Visibility Loss {loss_v.val:.4f} ({loss_v.avg:.4f})\t'
-				'Location Loss {loss_l.val:.4f} ({loss_l.avg:.4f})\t'
-				'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(epoch, i, len(train_loader), batch_time=batch_time,
-				data_time=data_time, loss_v=losses_v, loss_l = losses_l, top1=top1))
-
 
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
